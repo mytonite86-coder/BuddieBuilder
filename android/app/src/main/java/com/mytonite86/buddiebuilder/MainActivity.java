7b53b772a297a3db0c92ca69b92d6eff98d0075c
mytonite86-coder <mytonite86@gmail.com>
2026-05-18 08:22:05 -0600

    Added Android Capacitor build support

---
+++ android/app/src/main/java/com/mytonite86/buddiebuilder/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mytonite86.buddiebuilder;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mytonite86.buddiebuilder',
+  appName: 'BuddieBuilder',
+  webDir: 'dist'
+};
+
+export default config;
