5e2a53b8c5c9e4d6dfe1e4576dddb47960409bec
mytonite86-coder <mytonite86@gmail.com>
2026-05-19 14:04:42 -0600

    Updated package.json

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mytonite86.buddiebuilder',
-  appName: 'BuddieBuilder',
-  webDir: 'dist'
-};
-
-export default config;
